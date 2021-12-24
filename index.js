843f207f433567d0730cba40c0f7cd14ca8292e1
Loan-Shark <38702035+Loan-Shark@users.noreply.github.com>
2021-12-24 09:57:20 -0800

    pain

---
+++ index.js
@@ -0,0 +1,10 @@
+const fsp = require('fs').promises;
+const demoFileLocation = "D:/Steam/steamapps/common/Team Fortress 2/tf/demos/";
+const killStreakIndexLocation = "_events.txt";
+const re = /^\>\n[^\"]*\"([^\"]*)\"/gm;
+
+(async () => {
+    const readKillStreakIndex = await fsp.readFile(demoFileLocation + killStreakIndexLocation,'utf8');
+    const killStreakIndex = await readKillStreakIndex.match(re);
+    console.log(killStreakIndex);
+})();
