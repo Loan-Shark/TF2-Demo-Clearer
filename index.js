8cdfc66a3de47eb0362957245728f599f54fffce
Loan-Shark <38702035+Loan-Shark@users.noreply.github.com>
2022-01-05 14:22:49 -0800

    better boolean logic

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const re = /^>[^"]*"([^"]*)/gm;
 
 (async () => {
     try {
-        const [files , streakIndex] = await Promise.all([
+        let [files , streakIndex] = await Promise.all([
             fsp.readdir(demoFileLocation),
             fsp.readFile(demoFileLocation + eventsFile , 'utf8')
         ]);
+        files.splice(files.indexOf(eventsFile),1);
         const parsedStreaks = [...streakIndex.matchAll(re)].map(column => column[1]);
         for (const file of files)
-            if (!parsedStreaks.includes(file.replace(/.(dem|json)/,'')) && file != '_events.txt')
+            if (!parsedStreaks.includes(file.replace(/.(dem|json)/,'')))
                 fsp.rm(demoFileLocation + file);
     } catch (err) {
         console.error(err);
