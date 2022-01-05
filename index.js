33895f3ac79d4656a4d1d0fed1a520d0c13136ae
Loan-Shark <38702035+Loan-Shark@users.noreply.github.com>
2022-01-05 14:09:31 -0800

    fixed it :D

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fsp = require('fs').promises;
 const demoFileLocation = "D:/Steam/steamapps/common/Team Fortress 2/tf/demos/";
 const eventsFile = "_events.txt";
-const re = /"([^"]*)"/gm;
+const re = /^>[^"]*"([^"]*)/gm;
 
 (async () => {
     try {
@@ -9,13 +9,10 @@ const re = /"([^"]*)"/gm;
             fsp.readdir(demoFileLocation),
             fsp.readFile(demoFileLocation + eventsFile , 'utf8')
         ]);
-        const parsedStreaks = [...new Set([...streakIndex.matchAll(re)].map(column => column[1]))];
-        console.log(parsedStreaks);
-        /*
+        const parsedStreaks = [...streakIndex.matchAll(re)].map(column => column[1]);
         for (const file of files)
             if (!parsedStreaks.includes(file.replace(/.(dem|json)/,'')) && file != '_events.txt')
                 fsp.rm(demoFileLocation + file);
-        */
     } catch (err) {
         console.error(err);
     }
